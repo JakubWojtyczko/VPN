f537ab8f605010dcca2a8b00d5977f28ddfb711d
Jakub Wojtyczko <jakub.wojtyczko@nokia.com>
2019-10-10 17:43:30 +0200

    complete ip header

--- IP/IPv4.h
+++ IP/IPv4.h
@@ -3,18 +3,29 @@
 
 #include <cstdint>
 
-namespace vpn
-{
+namespace vpn {
+
 
 struct IPv4Header {
-    std::uint8_t addr;
-    std::uint8_t control;
-    std::uint16_t protocol_id;
-    std::uint8_t code;
+    // octet 1
+    std::uint8_t version_ihl;
+    std::uint8_t type_of_service;
     std::uint16_t length;
-    
+    // octet 2
+    std::uint16_t identification;
+    std::uint16_t df_dm_fragment_offset;
+    // octet 3
+    std::uint8_t ttl;
+    std::uint8_t protocol;
+    std::uint16_t header_checksum;
+    // octet 4
     std::uint32_t source_address;
+    // octet 5
     std::uint32_t destination_address;
+    // octet 6
+    std::uint32_t options;
+	
+    /* data here */
 };
 
 } // namespace vpn
